e6267c72677dc16b7216bf65bec986b1e78b8142
MehediHasan128 <mehedihasan12926@gmail.com>
2025-04-19 19:47:51 +0600

    create user validation schema

--- src/app/modules/users/user.validation.ts
+++ src/app/modules/users/user.validation.ts
@@ -1,6 +1,12 @@
 import { z } from "zod"
 
-const createUserValidationSchema = z.object({})
+const createUserValidationSchema = z.object({
+    password: z.string()
+    .min(8, {message: 'Password must be at least 8 characters long'})
+    .regex(/[A-Z]/, {message: 'Password must contain at least one uppercase letter'})
+    .regex(/[0-9]/, {message: 'Password must contain at least one number'})
+    .regex(/[!@#$%^&*(),.?":{}|<>]/, {message: 'Password must contain at least one special character'})
+})
 
 export const UserValidation = {
     createUserValidationSchema
