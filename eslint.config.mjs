50a2840448f91b49ad86137005c9a6a6d3b5cabe
MehediHasan128 <mehedihasan12926@gmail.com>
2025-04-19 11:07:19 +0600

    connect server and setup eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,29 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    languageOptions: { globals: globals.browser },
+  },
+  {
+    rules: {
+      eqeqeq: "off",
+      "no-unused-vars": "error",
+      "prefer-const": ["error", { ignoreReadBeforeAssign: true }],
+    },
+  },
+  {
+    ignores: [".node_modules/**", "dist/**"],
+  },
+  tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+]);
---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import express, { Application } from 'express';
+import cors from 'cors';
+
+const app: Application = express();
+
+// Perser
+app.use(express.json());
+app.use(cors());
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+export default app;
