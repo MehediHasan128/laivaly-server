442e626e81b7528ed21e88f6e8dd7993837f86d1
MehediHasan128 <mehedihasan12926@gmail.com>
2025-04-19 14:19:20 +0600

    create global error handler function to catch all error from globaly

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,19 @@
-import express, { Application } from 'express';
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import express, { Application, NextFunction, Request, Response } from 'express';
 import cors from 'cors';
+import globalErrorHandler from './app/middlwares/globalErrorHandler';
 
 const app: Application = express();
 
 // Perser
 app.use(express.json());
 app.use(cors());
-const port = 3000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+// Use middlwaer
+app.use(globalErrorHandler as (err: any, req: Request, res: Response, next: NextFunction) => void);
 
 export default app;
