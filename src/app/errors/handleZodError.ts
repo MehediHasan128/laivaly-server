acf1f6d7c31e2092199979350313ddd918c77a3f
MehediHasan128 <mehedihasan12926@gmail.com>
2025-04-20 19:44:56 +0600

    create zod global error handler

---
+++ src/app/errors/handleZodError.ts
@@ -0,0 +1,23 @@
+import { ZodError, ZodIssue } from "zod";
+import { TErrorSource, TGenericErrorResponce } from "../global/error.interface";
+
+const handleZodError = (err: ZodError): TGenericErrorResponce => {
+
+    const errorSource: TErrorSource = err.issues.map((issue: ZodIssue) => {
+        return {
+            path: issue?.path[issue?.path.length - 1],
+            message: issue?.message
+        }
+    });
+
+    const statusCode = 400;
+
+    return {
+        statusCode,
+        message: 'Zod validation error',
+        errorSource
+    }
+
+};
+
+export default handleZodError;
--- src/app/middlwares/globalErrorHandler.ts
+++ src/app/middlwares/globalErrorHandler.ts
@@ -1,11 +1,26 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { NextFunction, Request, Response } from "express";
+import { TErrorSource } from "../global/error.interface";
+import { ZodError } from "zod";
+import handleZodError from "../errors/handleZodError";
 
 const globalErrorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
 
-    const statusCode = 500;
-    const message = 'Something went wrong';
+    let statusCode = 500;
+    let message = 'Something went wrong';
+    let errorSource: TErrorSource = [{
+        path: '',
+        message: 'Something went wrong'
+    }];
+
+
+    if(err instanceof ZodError) {
+        const simplifiedError = handleZodError(err);
+        statusCode = simplifiedError?.statusCode;
+        message = simplifiedError?.message;
+        errorSource = simplifiedError?.errorSource;
+    }
 
 
     return res.status(statusCode).json({
