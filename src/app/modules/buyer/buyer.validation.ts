61f6a572c2a4f4daddbf1ced3a37eee0d7b7715a
MehediHasan128 <mehedihasan12926@gmail.com>
2025-04-20 17:17:06 +0600

    update buyer information schema

--- src/app/modules/buyer/buyer.validation.ts
+++ src/app/modules/buyer/buyer.validation.ts
@@ -60,6 +60,53 @@ const createBuyerValidationSchema = z.object({
   }),
 });
 
+const updateBuyerValidationSchema = z.object({
+  body: z.object({
+    userName: UserNameValidationSchema.optional(),
+
+    userEmail: z
+      .string({
+        required_error: 'Email address is required',
+        invalid_type_error: 'Email address must be a string',
+      })
+      .email('Please provide a valid email address')
+      .trim()
+      .toLowerCase()
+      .optional(),
+
+    profileImage: z.string().optional().default(''),
+
+    dateOfBirth: z
+      .string({
+        required_error: 'Date of birth is required',
+      })
+      .refine((val) => !isNaN(Date.parse(val)), {
+        message: 'Invalid date format',
+      })
+      .optional(),
+
+    gender: z
+      .enum(['male', 'female'], {
+        required_error: 'Gender is required',
+        invalid_type_error: 'Gender must be either male or female',
+      })
+      .optional(),
+
+    phoneNumber: z
+      .string({
+        required_error: 'Phone number is required',
+      })
+      .regex(/^\+?[1-9]\d{1,14}$/, 'Phone number is invalid')
+      .optional(),
+
+    shippingAddress: z
+      .array(ShippingAddressValidation)
+      .min(1, 'At least one shipping address is required')
+      .optional(),
+  }),
+});
+
 export const BuyerValidation = {
   createBuyerValidationSchema,
+  updateBuyerValidationSchema
 };
