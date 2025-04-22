56f1f1a9726842cd0a1990eb96b6a7ae3b255695
MehediHasan128 <mehedihasan12926@gmail.com>
2025-04-22 08:00:25 +0600

    create send email function and create a reset email templates

---
+++ src/app/utils/sendMail.ts
@@ -0,0 +1,25 @@
+import nodemailer from 'nodemailer';
+import config from '../config';
+
+const sendMail = async(to: string, subject: string, link: string, htmlPage: string) => {
+
+    const transporter = nodemailer.createTransport({
+        host: 'smtp.gmail.com',
+        port: 587,
+        secure: false,
+        auth: {
+            user: 'bayzidahmed467@gmail.com',
+            pass: config.smtp_secret_credential
+        }
+    });
+
+    await transporter.sendMail({
+        from: 'bayzidahmed467@gmail.com',
+        to,
+        subject: subject,
+        html: htmlPage
+    })
+
+};
+
+export default sendMail;
