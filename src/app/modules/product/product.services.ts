dd909177a267b47347c40b8af19b1123fc801661
MehediHasan128 <mehedihasan12926@gmail.com>
2025-04-24 23:48:54 +0600

    upload multiple image

--- src/app/modules/product/product.services.ts
+++ src/app/modules/product/product.services.ts
@@ -1,6 +1,19 @@
+import { uploadImageToCloudinary } from "../../utils/uploadImageToCloudinary";
+import { TProduct } from "./product.interface";
+
 /* eslint-disable @typescript-eslint/no-explicit-any */
-const addProductIntoDB = async(file: any, payload: string) => {
-    console.log(file);
+const addProductIntoDB = async(file: any, payload: TProduct) => {
+
+    const imagePath = file.map((image: any) => ({
+        imagePath: image.path,
+        imageName: image.filename
+    }));
+
+    const uploadedFiles = await uploadImageToCloudinary(imagePath);
+
+    payload.thumbnail = uploadedFiles[0].secure_url;
+    payload.images = uploadedFiles.map((file) => file.secure_url);
+
     console.log(payload);
 }
 
--- src/app/utils/uploadImageToCloudinary.ts
+++ src/app/utils/uploadImageToCloudinary.ts
@@ -1,41 +1,49 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { v2 as cloudinary } from 'cloudinary';
 import config from '../config';
 import fs from 'fs';
 import multer from 'multer';
 
-export const uploadImageToCloudinary = (imagePath: string, imageName: string) => {
-  cloudinary.config({
-    cloud_name: config.cloudinary_cloud_name,
-    api_key: config.cloudinary_api_key,
-    api_secret: config.cloudinary_api_secret,
-  });
+cloudinary.config({
+  cloud_name: config.cloudinary_cloud_name,
+  api_key: config.cloudinary_api_key,
+  api_secret: config.cloudinary_api_secret,
+});
 
-  return cloudinary.uploader.upload(
-    imagePath,
-    {
-        public_id: imageName
-    },
-    function(error, result) {
-        console.log(result);
-        fs.unlink(imagePath, (err) => {
+export const uploadImageToCloudinary = async (
+  files: { imagePath: string; imageName: string }[],
+) => {
+
+    const results: any[] = [];
+
+  const uploadPromises = files.map((file) =>
+    cloudinary.uploader
+      .upload(file.imagePath, { public_id: file.imageName })
+      .then((res) => {
+        results.push(res);
+        fs.unlink(file.imagePath, (err) => {
             if(err){
-                console.error(err);
+                console.log(err);
             }
         })
-    }
-  )
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+  );
+
+  await Promise.all(uploadPromises);
+  return results
 };
 
-
 const storage = multer.diskStorage({
-    destination: function(req, file, cb) {
-        cb(null, process.cwd() + '/uploads');
-    },
-    filename: function(req, file, cb) {
-        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
-        cb(null, file.fieldname + '-' + uniqueSuffix);
-    }
-})
+  destination: function (req, file, cb) {
+    cb(null, process.cwd() + '/uploads');
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
+    cb(null, file.fieldname + '-' + uniqueSuffix);
+  },
+});
 
-
-export const upload = multer({storage: storage});
+export const upload = multer({ storage: storage });
