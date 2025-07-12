10aec4984b9a903fef1ee7c346b6cda449ef55e9
MehediHasan128 <mehedihasan12926@gmail.com>
2025-07-12 23:24:56 +0600

    handle unhandledRejection or uncaughtException

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,17 @@
 import mongoose from 'mongoose';
 import app from './app';
 import config from './app/config';
+import { Server } from 'http';
+
+
+let server: Server;
 
 async function main() {
   try {
     await mongoose.connect(config.database_url as string);
 
-    app.listen(config.port, () => {
-      console.log(`Example app listening on port ${config.port}`);
+    server = app.listen(config.port, () => {
+      console.log(`The laivaly server is running on port ${config.port}`);
     });
   } catch (err) {
     console.log(err);
@@ -15,3 +19,21 @@ async function main() {
 }
 
 main();
+
+
+process.on('unhandledRejection', (err) => {
+  console.log(`unhandledRejection is detected, shutting down ...`, err);
+
+  if(server){
+    server.close(() => {
+      process.exit(1);
+    })
+  }
+  
+  process.exit(1);
+});
+
+process.on('uncaughtException', () => {
+  console.log(`uncaughtException is detected , shutting down ...`);
+  process.exit(1);
+})
