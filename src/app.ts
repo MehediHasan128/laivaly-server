6df52dc77f5524f1240238cd5022e3e278f02769
MehediHasan128 <mehedihasan12926@gmail.com>
2025-07-12 23:57:22 +0600

    create global error handler

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import cors from 'cors';
-import express, { Application } from 'express';
+import express, { Application, NextFunction, Request, Response } from 'express';
+import { globalErrorHandler } from './app/middlewares/globalErrorHandler';
 
 const app: Application = express();
 
@@ -10,5 +12,8 @@ app.use(cors());
 app.get('/', (req, res) => {
   res.send('The Laivaly server is run successfully')
 });
+
+// middlewares
+app.use(globalErrorHandler as (err: any, req: Request, res: Response, next: NextFunction) => void)
 
 export default app;
