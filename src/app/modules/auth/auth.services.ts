2130e63ab826cde3bcfad466d1eb53e4052a9c1b
MehediHasan128 <mehedihasan12926@gmail.com>
2025-07-13 14:42:14 +0600

    create the user is delete or banned

--- src/app/modules/auth/auth.services.ts
+++ src/app/modules/auth/auth.services.ts
@@ -9,6 +9,18 @@ const userLogin = async(payload: TUserLogin) => {
     const isUserExist = await User.findOne({userEmail: payload.userEmail});
     if(!isUserExist){
         throw new AppError(httpStatus.NOT_FOUND, 'User not found !!');
+    };
+
+    // Check the user is delete or not
+    const isUserDeleted = isUserExist.isDelete;
+    if(isUserDeleted){
+        throw new AppError(httpStatus.FORBIDDEN, 'User is already delete !!');
+    }
+
+    // Check the user is banned
+    const isUserbanned = isUserExist.status;
+    if(isUserbanned === 'banned'){
+        throw new AppError(httpStatus.FORBIDDEN, 'User is already banned !!');
     }
 
 };
